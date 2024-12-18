f7da68f5166053fa35177035c0974cf5a9aeb317
JulienBacquart <16917004+JulienBacquart@users.noreply.github.com>
2024-12-18 13:25:07 +0000

    add populateDatastore()

--- src/main/java/foo/Endpoint.java
+++ src/main/java/foo/Endpoint.java
@@ -1,5 +1,10 @@
 package foo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.api.server.spi.auth.common.User;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
@@ -10,6 +15,8 @@ import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+
+import com.opencsv.CSVReader;
 
 
 @Api(name = "myTinyApi",
@@ -52,4 +59,60 @@ public class Endpoint {
 	}
 
 	// TODO: addQuadSecure
+
+	// TODO: Make sure can only be run by someone logged
+	// Populate the DataStore from the dataset.csv file
+	// Be extremely carefull while calling it, if run a second time, will add the data in double
+	// URL follow the following form:
+	// https://cloud-tinyldf.appspot.com/_ah/api/myTinyApi/v1/populateDatastore?batchSize=1000
+	@ApiMethod(name = "populateDatastore", path = "populateDatastore", httpMethod = HttpMethod.GET)
+	public Entity populateDatastore(@Named("batchSize") int batchSize) {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		String csvFile = getClass().getClassLoader().getResource("dataset.csv").getPath();
+		int totalAdded = 0;
+
+		try (CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(csvFile)))) {
+			String[] row;
+			List<Entity> batch = new ArrayList<>();
+
+			// Skip header
+			csvReader.readNext();
+
+			while ((row = csvReader.readNext()) != null) {
+				// Create a Quad entity
+				Entity e = new Entity("Quad");
+				e.setProperty("subject", row[0]);
+				e.setProperty("predicate", row[1]);
+				e.setProperty("object", row[2]);
+				e.setProperty("graph", "http://example.org/graph/2024_medalists_all");
+
+				// Add to batch
+				batch.add(e);
+
+				// Process batch when it reaches the batch size
+				if (batch.size() >= batchSize) {
+					datastore.put(batch);
+					totalAdded += batch.size();
+					batch.clear(); // Clear batch
+				}
+			}
+
+			// Write remaining entities in the batch
+			if (!batch.isEmpty()) {
+				datastore.put(batch);
+				totalAdded += batch.size();
+			}
+
+			// Return summary
+			Entity result = new Entity("Result");
+			result.setProperty("elementsAdded", totalAdded);
+			return result;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			Entity errorEntity = new Entity("Error");
+			errorEntity.setProperty("message", "Error processing file: " + e.getMessage());
+			return errorEntity;
+		}
+	}
 }
